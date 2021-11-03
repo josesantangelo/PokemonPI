d82d95addd5ef277556e8a2a0f1214fa9bf9896b
Jose <josesantangelo93@gmail.com>
2021-11-03 19:55:04 -0300

    link en pokemons, reset estilos links

--- client/src/components/pokemon/Pokemon.jsx
+++ client/src/components/pokemon/Pokemon.jsx
@@ -1,12 +1,14 @@
 import s from "./pokemon.module.css";
-
+import { Link } from "react-router-dom";
 export default function Pokemon({ name, types, id, img }) {
   return (
-    <div id={s.card} className={types[0]}>
-      <h3>{name}</h3>
-      <h4>{types.join("/")}</h4>
-      <h4>{id}</h4>
-      <img src={img} alt="img poke" />
-    </div>
+    <Link to="/detail" className={s.link}>
+      <div id={s.card} className={types[0]}>
+        <h3>{name}</h3>
+        <h4>{types.join("/")}</h4>
+        <h4>{id}</h4>
+        <img src={img} alt="img poke" />
+      </div>
+    </Link>
   );
 }
